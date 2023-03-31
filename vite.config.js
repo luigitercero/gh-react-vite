925e8c8f1437924e49a2d89ab516edd473cb4330
Chato <luigitercero3@gmail.com>
2023-03-31 13:33:09 -0600

    arreglar acttion

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://luigitercero.github.io/',
+  base: 'https://luigitercero.github.io/gh-react-vite/',
   plugins: [react()],
 })
